4f792b3b86c20f23580532f9164256daae2cdc8e
albertomandirola <albymandi00@gmail.com>
2024-01-25 16:18:42 +0100

    create first page index.php

--- index.php
+++ index.php
@@ -6,6 +6,21 @@
     <title>Document</title>
 </head>
 <body>
+        
+            <form action="response.php" method="GET">
+                <div>
+                    <label for="paragraph"> Paragrafo: </label>
+                    <textarea name="paragrafo" id="paragraph" cols="30" rows="10"></textarea>
+                </div>
+                <div>
+                    <label for="bad_word">Parola da censurare:</label>
+                    <input type="text" name="bad_word" id="bad_word">
+                </div>
+                <div>
+                    <button type="submit">Censura</button>
+                </div>
+            </form>
+        
     
 </body>
 </html>
